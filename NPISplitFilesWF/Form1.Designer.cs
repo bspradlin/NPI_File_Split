357a17ab1395b53654366d51f6224adf0fb369ba
Brian Spradlin <bspradlin@xpanxion.com>
2013-03-12 15:03:10 -0500

    Initial upload of NPI file splitter.

---
+++ NPISplitFilesWF/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace NPISplitFilesWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourceFile = new System.Windows.Forms.TextBox();
+            this.sourceLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.destinationPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sourceFile
+            // 
+            this.sourceFile.Location = new System.Drawing.Point(12, 37);
+            this.sourceFile.Name = "sourceFile";
+            this.sourceFile.Size = new System.Drawing.Size(260, 20);
+            this.sourceFile.TabIndex = 0;
+            // 
+            // sourceLabel
+            // 
+            this.sourceLabel.AutoSize = true;
+            this.sourceLabel.Location = new System.Drawing.Point(12, 13);
+            this.sourceLabel.Name = "sourceLabel";
+            this.sourceLabel.Size = new System.Drawing.Size(87, 13);
+            this.sourceLabel.TabIndex = 1;
+            this.sourceLabel.Text = "NPI Source Path";
+            this.sourceLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Split Files Destination Path";
+            // 
+            // destinationPath
+            // 
+            this.destinationPath.Location = new System.Drawing.Point(15, 128);
+            this.destinationPath.Name = "destinationPath";
+            this.destinationPath.Size = new System.Drawing.Size(260, 20);
+            this.destinationPath.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Split File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.destinationPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sourceLabel);
+            this.Controls.Add(this.sourceFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sourceFile;
+        private System.Windows.Forms.Label sourceLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox destinationPath;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ NPISplitFilesWF/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NPISplitFilesWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int recordCounter = 0;
+            int fileNumCounter = 0;
+            int catchHeaderRow = 0;
+            string headerRow = "";
+            bool lineAdded = false;
+
+            using (var reader = new StreamReader(sourceFile.Text))
+            {
+                List<string> fileEntries = new List<string>();
+                List<string> CTEntries = new List<string>();
+                List<string> GAEntries = new List<string>();
+                List<string> TXEntries = new List<string>();
+                headerRow = reader.ReadLine();
+
+                fileEntries.Add(headerRow);
+                CTEntries.Add(headerRow);
+                GAEntries.Add(headerRow);
+                TXEntries.Add(headerRow);
+
+                while (!reader.EndOfStream)
+                {
+
+                    string readerLine = reader.ReadLine();
+                    List<string> readerArray = readerLine.Split(new string[] {"\",\""}, StringSplitOptions.None).ToList<string>();
+                    string readerArrayState = readerArray[23];
+
+                    if (readerArrayState == "CT")
+                    {
+                        CTEntries.Add(readerLine);
+                        fileEntries.Add(readerLine);
+                        lineAdded = true;
+                    }
+                    else if (readerArrayState == "GA")
+                    {
+                        GAEntries.Add(readerLine);
+                        fileEntries.Add(readerLine);
+                        lineAdded = true;
+                    }
+                    else if (readerArrayState == "TX")
+                    {
+                        TXEntries.Add(readerLine);
+                        fileEntries.Add(readerLine);
+                        lineAdded = true;
+                    }
+
+
+                    /*
+                    string toWrite = string.Join(Environment.NewLine, fileEntries.ToList());
+                    File.AppendAllText(string.Format(@"{0}\NPI_{1}_File.txt", destinationPath.Text, "CT"), toWrite);
+                    */
+
+                    if ((recordCounter >= 30000) && (!lineAdded))
+                    {
+                        fileEntries.Add(readerLine);
+                        recordCounter = 0;
+                    }
+
+                    lineAdded = false;
+
+                    //if (readerLine.Contains("\"CT\""))
+                    //{
+                    //    CTEntries.Add(readerLine);
+                    //    //fileEntries.Add(readerLine);
+                    //}
+                    //if (readerLine.Contains("\"GA\""))
+                    //{
+                    //    GAEntries.Add(readerLine);
+                    //}
+                    //if (readerLine.Contains("\"TX\""))
+                    //{
+                    //    TXEntries.Add(readerLine);
+                    //}
+
+                    //fileEntries.Add(reader.ReadLine());
+                    //if ((recordCounter >= 44000) || (reader.EndOfStream))
+                    if (reader.EndOfStream)
+                    {
+                        fileNumCounter++;
+                        //File.WriteAllLines(string.Format(@"{0}\NPI_CT_TX_GA_File{1}.txt", destinationPath.Text, fileNumCounter), fileEntries.ToList());
+                        File.WriteAllLines(string.Format(@"{0}\NPI_CT_File.txt", destinationPath.Text), CTEntries.ToList());
+                        File.WriteAllLines(string.Format(@"{0}\NPI_GA_File.txt", destinationPath.Text), GAEntries.ToList());
+                        File.WriteAllLines(string.Format(@"{0}\NPI_TX_File.txt", destinationPath.Text), TXEntries.ToList());
+                        File.WriteAllLines(string.Format(@"{0}\NPI_CT_GA_TX_File.txt", destinationPath.Text), fileEntries.ToList());
+                        fileEntries = new List<string>();
+                        recordCounter = 0;
+                    }
+                }
+            }
+        }
+    }
+}
