9248900fdccd74d6d15a2d7600da8ae244627446
Brian Spradlin <bspradlin@xpanxion.com>
2013-03-12 16:19:47 -0500

    Updated code to allow user to enter state abbreviation for file creation.

--- NPISplitFilesWF/Form1.Designer.cs
+++ NPISplitFilesWF/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.destinationPath = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.stateSelect = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // sourceFile
@@ -55,7 +57,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 102);
+            this.label1.Location = new System.Drawing.Point(12, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(132, 13);
             this.label1.TabIndex = 2;
@@ -63,14 +65,14 @@
             // 
             // destinationPath
             // 
-            this.destinationPath.Location = new System.Drawing.Point(15, 128);
+            this.destinationPath.Location = new System.Drawing.Point(12, 97);
             this.destinationPath.Name = "destinationPath";
             this.destinationPath.Size = new System.Drawing.Size(260, 20);
             this.destinationPath.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(15, 163);
+            this.button1.Location = new System.Drawing.Point(12, 207);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -78,11 +80,29 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "State to extract";
+            // 
+            // stateSelect
+            // 
+            this.stateSelect.Location = new System.Drawing.Point(12, 168);
+            this.stateSelect.Name = "stateSelect";
+            this.stateSelect.Size = new System.Drawing.Size(75, 20);
+            this.stateSelect.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.stateSelect);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.destinationPath);
             this.Controls.Add(this.label1);
@@ -102,6 +122,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox destinationPath;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox stateSelect;
     }
 }
 
--- NPISplitFilesWF/Form1.cs
+++ NPISplitFilesWF/Form1.cs
@@ -26,91 +26,112 @@ namespace NPISplitFilesWF
         private void button1_Click(object sender, EventArgs e)
         {
             int recordCounter = 0;
-            int fileNumCounter = 0;
-            int catchHeaderRow = 0;
+            //int fileNumCounter = 0;
             string headerRow = "";
-            bool lineAdded = false;
+            //bool lineAdded = false;
 
-            using (var reader = new StreamReader(sourceFile.Text))
+            if (!string.IsNullOrEmpty(stateSelect.Text))
             {
-                List<string> fileEntries = new List<string>();
-                List<string> CTEntries = new List<string>();
-                List<string> GAEntries = new List<string>();
-                List<string> TXEntries = new List<string>();
-                headerRow = reader.ReadLine();
 
-                fileEntries.Add(headerRow);
-                CTEntries.Add(headerRow);
-                GAEntries.Add(headerRow);
-                TXEntries.Add(headerRow);
-
-                while (!reader.EndOfStream)
+                using (var reader = new StreamReader(sourceFile.Text))
                 {
+                    List<string> fileEntries = new List<string>();
+                    headerRow = reader.ReadLine();
 
-                    string readerLine = reader.ReadLine();
-                    List<string> readerArray = readerLine.Split(new string[] {"\",\""}, StringSplitOptions.None).ToList<string>();
-                    string readerArrayState = readerArray[23];
+                    fileEntries.Add(headerRow);
 
-                    if (readerArrayState == "CT")
+                    while (!reader.EndOfStream)
                     {
-                        CTEntries.Add(readerLine);
-                        fileEntries.Add(readerLine);
-                        lineAdded = true;
+
+                        string readerLine = reader.ReadLine();
+                        List<string> readerArray = readerLine.Split(new string[] { "\",\"" }, StringSplitOptions.None).ToList<string>();
+                        string readerArrayState = readerArray[23];
+
+                        if (readerArrayState == stateSelect.Text.ToString())
+                        {
+                            fileEntries.Add(readerLine);
+                            //recordCounter++;
+                        }
+
+                        /*
+                        if (recordCounter >= 1000)
+                        {
+                            string toWrite = string.Join(Environment.NewLine, fileEntries.ToList()) + Environment.NewLine;
+                            File.AppendAllText(string.Format(@"{0}\NPI_{1}_File.txt", destinationPath.Text, stateSelect.Text.ToString()), toWrite);
+                            recordCounter = 0;
+                            fileEntries = new List<string>();
+                        }
+                         */
+
+                        /*
+                        if (readerArrayState == "CT")
+                        {
+                            CTEntries.Add(readerLine);
+                            fileEntries.Add(readerLine);
+                            lineAdded = true;
+                        }
+                        else if (readerArrayState == "GA")
+                        {
+                            GAEntries.Add(readerLine);
+                            fileEntries.Add(readerLine);
+                            lineAdded = true;
+                        }
+                        else if (readerArrayState == "TX")
+                        {
+                            TXEntries.Add(readerLine);
+                            fileEntries.Add(readerLine);
+                            lineAdded = true;
+                        }
+                        */
+
+                        /*
+                        string toWrite = string.Join(Environment.NewLine, fileEntries.ToList());
+                        File.AppendAllText(string.Format(@"{0}\NPI_{1}_File.txt", destinationPath.Text, "CT"), toWrite);
+                        */
+
+                        /*
+                        if ((recordCounter >= 30000) && (!lineAdded))
+                        {
+                            fileEntries.Add(readerLine);
+                            recordCounter = 0;
+                        }
+
+                        lineAdded = false;
+                        */
+
+                        //if (readerLine.Contains("\"CT\""))
+                        //{
+                        //    CTEntries.Add(readerLine);
+                        //    //fileEntries.Add(readerLine);
+                        //}
+                        //if (readerLine.Contains("\"GA\""))
+                        //{
+                        //    GAEntries.Add(readerLine);
+                        //}
+                        //if (readerLine.Contains("\"TX\""))
+                        //{
+                        //    TXEntries.Add(readerLine);
+                        //}
+
+                        //fileEntries.Add(reader.ReadLine());
+                        //if ((recordCounter >= 44000) || (reader.EndOfStream))
+                        //if (reader.EndOfStream)
+                        //{
+                        //    fileNumCounter++;
+                        //    //File.WriteAllLines(string.Format(@"{0}\NPI_CT_TX_GA_File{1}.txt", destinationPath.Text, fileNumCounter), fileEntries.ToList());
+                        //    File.WriteAllLines(string.Format(@"{0}\NPI_CT_File.txt", destinationPath.Text), CTEntries.ToList());
+                        //    File.WriteAllLines(string.Format(@"{0}\NPI_GA_File.txt", destinationPath.Text), GAEntries.ToList());
+                        //    File.WriteAllLines(string.Format(@"{0}\NPI_TX_File.txt", destinationPath.Text), TXEntries.ToList());
+                        //    File.WriteAllLines(string.Format(@"{0}\NPI_CT_GA_TX_File.txt", destinationPath.Text), fileEntries.ToList());
+                        //    fileEntries = new List<string>();
+                        //    recordCounter = 0;
+                        //}
                     }
-                    else if (readerArrayState == "GA")
-                    {
-                        GAEntries.Add(readerLine);
-                        fileEntries.Add(readerLine);
-                        lineAdded = true;
-                    }
-                    else if (readerArrayState == "TX")
-                    {
-                        TXEntries.Add(readerLine);
-                        fileEntries.Add(readerLine);
-                        lineAdded = true;
-                    }
 
+                    File.WriteAllLines(string.Format(@"{0}\NPI_{1}_File.txt", destinationPath.Text, stateSelect.Text.ToString()), fileEntries.ToList());
 
-                    /*
-                    string toWrite = string.Join(Environment.NewLine, fileEntries.ToList());
-                    File.AppendAllText(string.Format(@"{0}\NPI_{1}_File.txt", destinationPath.Text, "CT"), toWrite);
-                    */
-
-                    if ((recordCounter >= 30000) && (!lineAdded))
-                    {
-                        fileEntries.Add(readerLine);
-                        recordCounter = 0;
-                    }
-
-                    lineAdded = false;
-
-                    //if (readerLine.Contains("\"CT\""))
-                    //{
-                    //    CTEntries.Add(readerLine);
-                    //    //fileEntries.Add(readerLine);
-                    //}
-                    //if (readerLine.Contains("\"GA\""))
-                    //{
-                    //    GAEntries.Add(readerLine);
-                    //}
-                    //if (readerLine.Contains("\"TX\""))
-                    //{
-                    //    TXEntries.Add(readerLine);
-                    //}
-
-                    //fileEntries.Add(reader.ReadLine());
-                    //if ((recordCounter >= 44000) || (reader.EndOfStream))
-                    if (reader.EndOfStream)
-                    {
-                        fileNumCounter++;
-                        //File.WriteAllLines(string.Format(@"{0}\NPI_CT_TX_GA_File{1}.txt", destinationPath.Text, fileNumCounter), fileEntries.ToList());
-                        File.WriteAllLines(string.Format(@"{0}\NPI_CT_File.txt", destinationPath.Text), CTEntries.ToList());
-                        File.WriteAllLines(string.Format(@"{0}\NPI_GA_File.txt", destinationPath.Text), GAEntries.ToList());
-                        File.WriteAllLines(string.Format(@"{0}\NPI_TX_File.txt", destinationPath.Text), TXEntries.ToList());
-                        File.WriteAllLines(string.Format(@"{0}\NPI_CT_GA_TX_File.txt", destinationPath.Text), fileEntries.ToList());
-                        fileEntries = new List<string>();
-                        recordCounter = 0;
-                    }
+                    //string toFinalWrite = string.Join(Environment.NewLine, fileEntries.ToList()) + Environment.NewLine;
+                    //File.AppendAllText(string.Format(@"{0}\NPI_{1}_File.txt", destinationPath.Text, stateSelect.Text.ToString()), toFinalWrite);
                 }
             }
         }
